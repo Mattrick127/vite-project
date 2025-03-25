8094933d43e431cadebb1da29153b04abfadf475
Matthew D Wilson <mattrick2214@gmail.com>
2025-03-24 21:39:14 -0500

    wrote in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import OrderDetails from './components/OrderDetails';
 
 function App() {
 
@@ -116,32 +117,10 @@ function App() {
                     </div>
                     
                 ) }
-                
+
             </section>
             
-
-            <section className="summary">
-                <strong>Order Details</strong>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Item</th>
-                            <th>Total</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1x Real Madrid</td>
-                            <td>$ 119.99</td>
-                        </tr>
-                        
-                        <tr>
-                            <th>Total</th>
-                            <th>$ 119.99</th>
-                        </tr>
-                    </tbody>
-                </table>
-            </section>
+            <OrderDetails />
             
         </>
     );
