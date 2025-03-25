fa5b6c2ea1804dcd51673e6b640c661eeea127c6
Matthew D Wilson <mattrick2214@gmail.com>
2025-03-24 21:18:28 -0500

    ternary operator for button

--- src/App.jsx
+++ src/App.jsx
@@ -103,11 +103,15 @@ function App() {
                         <div className="description">
                             <span className="name">{item.name}</span>
                             <span className="price">$ {item.price}</span>
+                    { item.isInBag ?
                             <div className="quantity-area">
                                 <button>-</button>
                                 <span className="quantity">{item.quantity}</span>
                                 <button>+</button>
                             </div>
+
+                            : ''
+                    }
                         </div>
                     </div>
                     
